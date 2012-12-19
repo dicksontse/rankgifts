d5fc383b834678e6abeef08803b0f814f1cb3c47
Dickson Tse <dtse87@gmail.com>
2012-12-18 22:49:57 -0800

    Add initial files

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>RankGifts</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="navbar navbar-fixed-top navbar-inverse">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="brand" href="index.php">RankGifts</a>
+                    <ul class="nav">
+                        <li><a href="#">Top 100</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="wrap">
+            <div id="main" class="container">
+                <h3>Welcome!</h3>
+                <p class="lead">
+                    
+                </p>
+            </div>
+            <div id="push"></div>
+        </div>
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="span3">
+                        <p class="muted"><a href="http://dicksontse.com" target="_blank">dicksontse.com</a></p>
+                    </div>
+                    <div id="credit" class="span9">
+                        <p>&copy; 2012 Dickson Tse</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
