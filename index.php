46620f180c27a8ad559ba560ec9b809b0e33b29c
Dickson Tse <dtse87@gmail.com>
2012-12-27 12:54:24 -0800

    Add Google Analytics

--- index.php
+++ index.php
@@ -10,6 +10,19 @@
         <link href="css/main.css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-37299618-1']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+        </script>
         <script>
           $(document).ready(function () {
             $("[rel=popover]").popover({'placement':'bottom', 'trigger':'focus'});
